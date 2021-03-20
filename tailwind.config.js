9cd593b4fd2adc8ff1c52228ddbba13c2783825e
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2021-03-20 18:59:08 -0500

    [chore] config eslint, tailwind and prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,30 +19,19 @@ module.exports = {
         '"Noto Color Emoji"',
       ],
       serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
-      mono: [
-        'Fira Mono',
-        'Menlo',
-        'Monaco',
-        'Consolas',
-        '"Liberation Mono"',
-        '"Courier New"',
-        'monospace',
-      ],
+      mono: ['Fira Mono', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
     },
     fontWeight: {
       light: 300,
       regular: 400,
       'semi-bold': 600,
       bold: 700,
-      'extra-bold': 800
+      'extra-bold': 800,
     },
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-}
+  plugins: [require('tailwindcss')],
+};
