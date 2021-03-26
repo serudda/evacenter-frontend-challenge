cc2567f419173bf52b059ac1143d92978a1c0307
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2021-03-26 16:28:34 -0500

    [refactor] add some unit tests

--- src/app/components/atoms/Cards/RiskCard/RiskCard.test.tsx
+++ src/app/components/atoms/Cards/RiskCard/RiskCard.test.tsx
@@ -6,4 +6,19 @@ describe('<RiskCard/>', () => {
     const { getByTestId } = render(<RiskCard type={Type.default} />);
     expect(getByTestId('RiskCard')).toBeInTheDocument();
   });
+
+  test('should show a Low message', () => {
+    const { getByTestId } = render(<RiskCard type={Type.positive} />);
+    expect(getByTestId('RiskCard')).toHaveTextContent('Low');
+  });
+
+  test('should show a Mid message', () => {
+    const { getByTestId } = render(<RiskCard type={Type.warning} />);
+    expect(getByTestId('RiskCard')).toHaveTextContent('Mid');
+  });
+
+  test('should show a High message', () => {
+    const { getByTestId } = render(<RiskCard type={Type.negative} />);
+    expect(getByTestId('RiskCard')).toHaveTextContent('High');
+  });
 });
--- src/app/components/atoms/Stat/Stat.test.tsx
+++ src/app/components/atoms/Stat/Stat.test.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Stat from './Stat';
 
 describe('<Stat/>', () => {
   test('renders Stat component', () => {
     const { getByTestId } = render(<Stat description="" />);
     expect(getByTestId('Stat')).toBeInTheDocument();
+  });
+
+  test('should show the value on screen', () => {
+    const { getByTestId } = render(<Stat description="" value={24.9} />);
+    expect(getByTestId('Stat')).toHaveTextContent('24.9');
   });
 });
